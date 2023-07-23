432b81aa9657ee024cf3786c6ccde8463699bf25
Mohamed Gado <boogado@yahoo.com>
2023-07-23 18:28:53 +0300

    finished landing  page

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { useAuth } from '@clerk/nextjs'
+import Marquee from 'react-fast-marquee'
 import TypewriterComponent from 'typewriter-effect'
 
 import { Button } from '@/components/ui/button'
@@ -10,7 +11,7 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth()
 
   return (
-    <div className="min-h-[calc(100vh-6rem)] space-y-5 py-36 text-center font-bold text-white">
+    <div className="min-h-[calc(100vh-6rem)] space-y-12 py-36 text-center font-bold text-white">
       <div className="space-y-5 text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl">
         <h1>The Best AI Tool for</h1>
         <div className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
@@ -44,6 +45,10 @@ export const LandingHero = () => {
       <div className="text-xs font-normal text-zinc-400 md:text-sm">
         No credit card required.
       </div>
+      <Marquee speed={70} className="tracking-widest">
+        Next Generation AI Powered by OpenAI - Generate Images, Text, Code,
+        Video, Audio, and more.
+      </Marquee>
     </div>
   )
 }
